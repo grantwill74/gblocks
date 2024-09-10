8aa5aa6a88fe0768a4d663d8af8d9f4eb5bfa403
grant.will.74 <grant@fakeemail.com>
2024-09-09 21:27:35 -0700

    pulsewave beginnings

--- src/audio.ts
+++ src/audio.ts
@@ -34,6 +34,51 @@ async function genWhiteNoise(context: AudioContext, duration: number):
     return buffer;
 }
 
+async function genPulseWave (
+    context: AudioContext, 
+    duty: number, 
+    freq: number,
+    duration: number
+) : Promise <AudioBuffer>
+{
+    const buffer = context.createBuffer (
+        1, SAMPLE_RATE * duration, SAMPLE_RATE);
+    
+    const buf_data = buffer.getChannelData (0);
+    const seconds_per_cycle = 1 / freq;
+    const samples_per_cycle = seconds_per_cycle * SAMPLE_RATE;
+    const lo_pulse_length = samples_per_cycle * (duty % 1);
+    const lo_pulse_error = lo_pulse_length % 1;
+    const hi_pulse_length = samples_per_cycle - lo_pulse_length;
+    const hi_pulse_error = hi_pulse_length % 1;
+
+    for (let i = 0; i < buf_data.length; i++) {
+        let i_s = i % samples_per_cycle;
+
+        if (i_s < lo_pulse_length) {
+            buf_data[i] = 0;
+        }
+        else if (i_s == (lo_pulse_length - lo_pulse_error)) {
+            if (Math.random () < lo_pulse_error) {
+                buf_data[i] = 0;
+            }
+            else {
+                buf_data[i] = 1;
+            }
+        }
+        else if (i_s > lo_pulse_length && 
+            i_s < (samples_per_cycle - (samples_per_cycle % 1))) 
+        {
+            buf_data[i] = 1;
+        }
+        else {
+            if (Math.random () < hi_pulse_error) {
+                buf_data[i] = 1;
+            }
+            else {
+                buf_data[i] = 0;
+            }
+        }
     }
 
     return buffer;
@@ -91,17 +136,25 @@ class SoundSys {
     context: AudioContext;
 
     crash_buf: AudioBuffer;
+    pulse_buf_50_c4: AudioBuffer;
 
     crash_inst: AdsrEnvelope;
+    doo_inst: AdsrEnvelope;
 
     master_gain: GainNode;
 
-    private constructor (context: AudioContext, crash_buf: AudioBuffer) {
+    private constructor (
+        context: AudioContext, 
+        crash_buf: AudioBuffer,
+        pulse_buf_50_c4: AudioBuffer
+    ) {
         this.context = context;
 
         this.crash_buf = crash_buf;
+        this.pulse_buf_50_c4 = pulse_buf_50_c4;
 
-        this.crash_inst = new AdsrEnvelope (context, 0, 1, 0, 1, 0.2);
+        this.crash_inst = new AdsrEnvelope (context, 0, 1, 0, 1, 0.15);
+        this.doo_inst = new AdsrEnvelope (context, 0, 1, 0.5, 0.5, 0.1);
 
         this.master_gain = context.createGain();
     }
@@ -109,9 +162,14 @@ class SoundSys {
     static async create (): Promise <SoundSys> {
         const context = createAudioContext();
 
-        const crash_buf = await genWhiteNoise (context, 5);
+        const crash_buf_prom = genWhiteNoise (context, 4);
+        const pulse_buf_prom = genPulseWave (context, 0.5, 261.6256, 4);
         
-        const sys = new SoundSys (context, crash_buf);
+        const sys = new SoundSys (
+            context, 
+            await crash_buf_prom,
+            await pulse_buf_prom
+        );
 
         sys.master_gain.gain.value = 0.25;
 
@@ -137,7 +195,7 @@ class SoundSys {
     }
 
     clear1 (): void {
-        
+        const clear_source = this.context.createBufferSource ();
     }
 }
 
